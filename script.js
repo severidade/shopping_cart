f36f3c24862cb17c88fb12c19a98ec4f36322315
severo <oliveira.severo@gmail.com>
2023-03-15 23:24:40 -0300

    refactor: :recycle: Retorna imagens maiores na busca

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const sumAllPrices = () => {
   currentCartItem.forEach((item) => { totalPrice += parseFloat(item.innerText.split('$')[1]); });
   priceElement.innerText = `${totalPrice}`;
   // coloquei o ToFixed para limitar a duas casas decimais o valor .toFixed()
+  // .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
 };
 
 function onLoadInfo() {
@@ -22,7 +23,6 @@ function onLoadInfo() {
   p.className = 'loading';
   p.innerText = 'Carregando...';
   pageLoad.appendChild(p);
-  // return p;
 }
 
 function emptyCart() {
@@ -70,50 +70,12 @@ function createCartItemElement({ sku, name, salePrice }) {
 
 async function addProductToCart(productID) {
   const itemData = await fetchItem(productID);
+  console.log(itemData);
   const sectionItem = document.querySelector('.cart__items');
   const { id: sku, title: name, price: salePrice } = itemData;
   const chartItem = createCartItemElement({ sku, name, salePrice });
   sectionItem.appendChild(chartItem);
 }
-
-// function createCartItemElement({ url, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${url} | NAME: ${name} | PRICE: ${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
-
-// async function addProductToCart(productID) {
-//   const itemData = await fetchItem(productID);
-//   const sectionItem = document.querySelector('.cart__items');
-//   const { thumbnail: url, title: name, price: salePrice } = itemData;
-//   const chartItem = createCartItemElement({ url, name, salePrice });
-//   sectionItem.appendChild(chartItem);
-// }
-
-// const waitLoading = document.querySelector('.loading');
-
-// function onLoadInfo() {
-//   const p = document.createElement('p');
-//   p.className = 'loading';
-//   p.innerText = 'Carregando...';
-//   return p;
-// }
-
-// function createProductItemElement({ sku, name, image, salePrice }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
-//   // section.appendChild(onLoadInfo());
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('span', 'priceProduct', `${salePrice}`));
-//   const buttonItem = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
-//   buttonItem.addEventListener('click', () => addProductToCart(sku));
-//   section.appendChild(buttonItem);
-//   return section;
-// }
 
 function createProductItemElement({ sku, name, image, salePrice }) {
   const section = document.createElement('section');
@@ -135,11 +97,15 @@ function createProductItemElement({ sku, name, image, salePrice }) {
 async function serchProducts(product) { // essa e uma funcao assincrona
   const searchData = await fetchProducts(product); // chamada da funcao fetchproduts.js
   const sectionItems = document.querySelector('.items'); // Target HTMl
+  console.log(searchData.results[0]);
+  
   searchData.results.forEach((item) => { // results é palcançar os dados no array
+    const newImageQuality = item.thumbnail.split('-');
+    const imageQuality = [...newImageQuality[0], '-', ...newImageQuality[1], '-J.jpg'].join('');
     const itemObject = {
       sku: item.id,
       name: item.title,
-      image: item.thumbnail,
+      image: imageQuality,
       salePrice: item.price,
     };
     const productItem = createProductItemElement(itemObject);
@@ -150,9 +116,9 @@ async function serchProducts(product) { // essa e uma funcao assincrona
 }
 
 window.onload = () => { 
-  serchProducts('Computador');
+  serchProducts('Boneco Funko');
   onLoadInfo();
   emptyCart();
   totalItem();
-  sumAllPrices();
+  // sumAllPrices(); isso acho que vai ter que ficar aqui para quando carregar do local storage
 };
