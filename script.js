56206cf81ea1d98f1f1b2883c0942eae2a4aada4
severo <oliveira.severo@gmail.com>
2023-03-17 21:15:11 -0300

    feat: :rocket: Adiciona imagem ao carrinho de comrpas

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+function onLoadInfo() {
+  const pageLoad = document.querySelector('.items');
+  const p = document.createElement('p');
+  p.className = 'loading';
+  p.innerText = 'Carregando...';
+  pageLoad.appendChild(p);
+}
+
 // Cria o conteiner para  inserir a soma
 function totalItem() {
+  const inicial_value = 0.00;
   const totalPrice = document.querySelector('.total');
   const span = document.createElement('span');
   span.className = 'total-price';
   totalPrice.appendChild(span);
+  span.innerText = `${inicial_value.toFixed(2)}`;
 }
 
 const sumAllPrices = () => {
@@ -13,14 +23,6 @@ const sumAllPrices = () => {
   currentCartItem.forEach((item) => { totalPrice += parseFloat(item.innerText.split('$')[1]); });
   priceElement.innerText = `${totalPrice.toFixed(2)}`;
 };
-
-function onLoadInfo() {
-  const pageLoad = document.querySelector('.items');
-  const p = document.createElement('p');
-  p.className = 'loading';
-  p.innerText = 'Carregando...';
-  pageLoad.appendChild(p);
-}
 
 function emptyCart() {
   const emptyButton = document.querySelector('.empty-cart');
@@ -39,15 +41,17 @@ function cartItemClickListener(event) {
 
 // cria os cards dos produtos
 function createProductImageElement(imageSource) {
-  const conteiner = document.createElement('div');
-  conteiner.className = 'conteiner_img';
-
+  const lowQltyImageURL = imageSource.split('-');
+  const highQltyImageURL = [...lowQltyImageURL[0], '-', ...lowQltyImageURL[1], '-J.jpg'].join('');
+  
+  const container = document.createElement('div');
+    container.className = 'container_img';
+  
   const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  conteiner.appendChild(img);
-  // return img;
-  return conteiner;
+    img.className = 'item__image';
+    img.src = highQltyImageURL;
+  container.appendChild(img);
+  return container;
 }
 
 function createCustomElement(element, className, innerText) {
@@ -57,11 +61,16 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ name, salePrice, imageSource }) {
   const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+    li.className = 'cart__item';
+
+  // const p = document.createElement('p');
+  //   li.appendChild(p);
+    li.innerText = `NAME: ${name} | PRICE: $${salePrice}`;
+    li.appendChild(createProductImageElement(imageSource));
+ 
+    li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
@@ -69,15 +78,15 @@ async function addProductToCart(productID) {
   const itemData = await fetchItem(productID);
   console.log(itemData.thumbnail);
   const sectionItem = document.querySelector('.cart__items');
-  const { id: sku, title: name, price: salePrice } = itemData;
-  const chartItem = createCartItemElement({ sku, name, salePrice });
+  const { id: sku, title: name, price: salePrice, thumbnail: imageSource } = itemData;
+  const chartItem = createCartItemElement({ sku, name, salePrice, imageSource });
   sectionItem.appendChild(chartItem);
 }
 
 function createProductItemElement({ sku, name, image, salePrice }) {
   const section = document.createElement('section');
   section.className = 'item';
-  // section.appendChild(onLoadInfo());
+
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('span', 'item__title', name));
@@ -97,12 +106,12 @@ async function serchProducts(product) { // essa e uma funcao assincrona
   console.log(searchData.results[0]);
   
   searchData.results.forEach((item) => { // results é palcançar os dados no array
-    const newImageQuality = item.thumbnail.split('-');
-    const imageQuality = [...newImageQuality[0], '-', ...newImageQuality[1], '-J.jpg'].join('');
+    // const newImageQuality = item.thumbnail.split('-');
+    // const imageQuality = [...newImageQuality[0], '-', ...newImageQuality[1], '-J.jpg'].join('');
     const itemObject = {
       sku: item.id,
       name: item.title,
-      image: imageQuality,
+      image: item.thumbnail,
       salePrice: item.price,
     };
     const productItem = createProductItemElement(itemObject);
@@ -129,7 +138,7 @@ async function serchProducts(product) { // essa e uma funcao assincrona
 // }
 
 window.onload = () => { 
-  serchProducts('monstera');
+  serchProducts('Monstera');
   onLoadInfo();
   emptyCart();
   totalItem();
