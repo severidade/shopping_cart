ff082d4f1423b3e07bd0c9984d8798dc1433adc1
severo <oliveira.severo@gmail.com>
2023-04-07 09:12:59 -0300

    refactor: :recycle: Refatorando funcao que abre e fecha menu.

--- js/shopping_cart.js
+++ js/shopping_cart.js
@@ -1,12 +1,22 @@
-// Alvo
-const menuControlOpen = document.getElementById('shopping_cart');
-const menuControlClose = document.getElementById('shopping_cart_close');
+// // Alvo
+// const menuControlOpen = document.getElementById('shopping_cart');
+// const menuControlClose = document.getElementById('shopping_cart_close');
 
-function toggleMenu() {
-  document.getElementById('cart').classList.toggle('active');
-  document.getElementsByTagName('body')[0].classList.toggle('boqueiaScroll');
-}
+// function toggleMenu() {
+//   document.getElementById('cart').classList.toggle('active');
+//   document.getElementsByTagName('body')[0].classList.toggle('boqueiaScroll');
+// }
 
-// Escutadores
-menuControlOpen.addEventListener('click', toggleMenu);
-menuControlClose.addEventListener('click', toggleMenu);
+// // Escutadores
+// menuControlOpen.addEventListener('click', toggleMenu);
+// menuControlClose.addEventListener('click', toggleMenu);
+
+document.querySelector('#shopping_cart').addEventListener('click', () => {
+  document.querySelector('#cart').classList.toggle('active');
+  document.body.classList.toggle('boqueiaScroll');
+});
+
+document.querySelector('#shopping_cart_close').addEventListener('click', () => {
+  document.querySelector('#cart').classList.toggle('active');
+  document.body.classList.toggle('boqueiaScroll');
+});
